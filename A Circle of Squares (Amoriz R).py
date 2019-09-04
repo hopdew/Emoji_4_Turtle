ee5deade1adc916830595f59bdcbdf72150da073
Runnels <100102324@lubbockisd.net>
2019-09-04 08:56:19 -0500

    Initial commit

---
+++ A Circle of Squares (Amoriz R).py
@@ -0,0 +1,16 @@
+import turtle
+turtle.speed(14325)
+turtle.bgcolor("grey")
+turtle.color("pink")
+def square():
+    for q in range(4):
+        turtle.forward(155)
+        turtle.right(90)
+
+for j in range(60):
+    square(100)
+    turtle.right(6)
+
+
+
+turtle.exitonclick()
---
+++ MY First Turtle.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.bgcolor("medium purple")
+turtle.shape("turtle")
+turtle.forward(31)
+turtle.right(60)
+turtle.color("pink")
+turtle.left(53)
+turtle.forward(64)
+
+turtle.backward(234)
+
+turtle.write("idk")
+turtle.forward(764)
+turtle.exitonclick()      
